db2fc2616a4813189048143da063ac7204b882f3
yisheng <yisheng.lim.2012@sis.smu.edu.sg>
2014-10-20 20:12:02 +0800

    Finished search

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,63 +1,139 @@
 /*jshint camelcase: false */
 (function () {
     'use strict';
-
-    var selectedSort = "";
-
-    $('.sort-bar-icon').click(function(){
-      $('.sort-bar-icon').removeClass('selected');
-      $(this).addClass('selected');
-      selectedSort = $(this).attr('value');
-    });
-
-    $('ul.cuisine-list li').hide().slice(0, 4).show();
-    var askForLoad = false;
+    var askForDietLoad = false;
+    var askForCuisineLoad = false;
+    var askForAmenityLoad = false;
+    var selectedDiet = [];
     var selectedCuisine = [];
+    var selectedAmenity = [];
 
-    $( '#cuisine-load' ).click(function() {
-      if (!askForLoad){
-        $('ul.cuisine-list li').show();
-        askForLoad = true;
-        $('#cuisine-load').html('Hide');
-      } else {
-        $('ul.cuisine-list li').hide().slice(0, 4).show();
-        askForLoad = false;
-        $('#cuisine-load').html('Load more');
-      }
+    $.getJSON( window.apiUrl + '/filters/list/', function( data ) {
+      var source = $('#cuisine-template').html();
+      var template = Handlebars.compile(source);
+      console.log(data);
+      var cuisines = {'cuisines': data.cuisines};
+      $('#cuisine').append(template(cuisines));
+
+      source = $('#diet-template').html();
+      template = Handlebars.compile(source);
+      var diets = {'diets': data.diets};
+      $('#diet').append(template(diets));
+
+      source = $('#amenity-template').html();
+      template = Handlebars.compile(source);
+      var amenities = {'amenities': data.amenities};
+      $('#amenity').append(template(amenities));
+
+      initialize();
+      
     });
 
-    $('.cuisine-list li').click(function(){
-      $(this).toggleClass('selected');
-      selectedCuisine = [];
-      $( '.cuisine-list li' ).each(function() {
-        if ($(this).hasClass('selected')){
-          selectedCuisine[selectedCuisine.length] = $(this).attr('value');
+    var selectedSort = '';
+
+    function initialize(){
+
+      $('.sort-bar-icon').click(function(){
+        $('.sort-bar-icon').removeClass('selected');
+        $(this).addClass('selected');
+        selectedSort = $(this).attr('value');
+      });
+
+      $('ul.diet-list li').hide().slice(0, 4).show();
+
+      $( '#diet-load' ).click(function() {
+        if (!askForDietLoad){
+          $('ul.diet-list li').show();
+          askForDietLoad = true;
+          $('#diet-load').html('Hide');
+        } else {
+          $('ul.diet-list li').hide().slice(0, 4).show();
+          askForDietLoad = false;
+          $('#diet-load').html('Load more');
         }
       });
-    });
 
-    $('.price-slider').noUiSlider({
-      start: [ 0, 1000 ],
-      connect: true,
-      range: {
-        'min': 0,
-        'max': 1000
-      }
-    });
+      $('.diet-list li').click(function(){
+        $(this).toggleClass('selected');
+        selectedDiet = [];
+        $( '.diet-list li' ).each(function() {
+          if ($(this).hasClass('selected')){
+            selectedDiet[selectedDiet.length] = $(this).attr('id');
+          }
+        });
+      });
 
-    $('.price-slider').Link('lower').to($('.price-display-low'));
-    $('.price-slider').Link('upper').to($('.price-display-high'));
+      $('ul.cuisine-list li').hide().slice(0, 4).show();
 
-    $('.location-slider').noUiSlider({
-      start: [15],
-      connect: 'lower',
-      range: {
-        'min': 0,
-        'max': 30
-      }
-    });
+      $( '#cuisine-load' ).click(function() {
+        if (!askForCuisineLoad){
+          $('ul.cuisine-list li').show();
+          askForCuisineLoad = true;
+          $('#cuisine-load').html('Hide');
+        } else {
+          $('ul.cuisine-list li').hide().slice(0, 4).show();
+          askForCuisineLoad = false;
+          $('#cuisine-load').html('Load more');
+        }
+      });
 
-    $('.location-slider').Link('lower').to($('.location-display-low'));
+      $('.cuisine-list li').click(function(){
+        $(this).toggleClass('selected');
+        selectedCuisine = [];
+        $( '.cuisine-list li' ).each(function() {
+          if ($(this).hasClass('selected')){
+            selectedCuisine[selectedCuisine.length] = $(this).attr('id');
+          }
+        });
+      });
+
+      $('ul.amenity-list li').hide().slice(0, 4).show();
+
+      $( '#amenity-load' ).click(function() {
+        if (!askForAmenityLoad){
+          $('ul.amenity-list li').show();
+          askForAmenityLoad = true;
+          $('#amenity-load').html('Hide');
+        } else {
+          $('ul.amenity-list li').hide().slice(0, 4).show();
+          askForAmenityLoad = false;
+          $('#amenity-load').html('Load more');
+        }
+      });
+
+      $('.amenity-list li').click(function(){
+        $(this).toggleClass('selected');
+        selectedAmenity = [];
+        $( '.amenity-list li' ).each(function() {
+          if ($(this).hasClass('selected')){
+            selectedAmenity[selectedAmenity.length] = $(this).attr('id');
+          }
+        });
+      });
+
+      $('.price-slider').noUiSlider({
+        start: [ 0, 1000 ],
+        connect: true,
+        range: {
+          'min': 0,
+          'max': 1000
+        }
+      });
+
+      $('.price-slider').Link('lower').to($('.price-display-low'));
+      $('.price-slider').Link('upper').to($('.price-display-high'));
+
+      $('.location-slider').noUiSlider({
+        start: [15],
+        connect: 'lower',
+        range: {
+          'min': 0,
+          'max': 30
+        }
+      });
+
+      $('.location-slider').Link('lower').to($('.location-display-low'));
+    }
 
 
 })();

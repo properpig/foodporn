65ac040700c2d76dc8e0456d5a2cbaa6099b62de
yisheng <yisheng.lim.2012@sis.smu.edu.sg>
2014-10-20 12:08:08 +0800

    Added toggle. And edited mistakes

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,18 +1,31 @@
 /*jshint camelcase: false */
 (function () {
     'use strict';
-    $('ul.cuisine-list li').hide().slice(0, 4).show();;
+    $('ul.cuisine-list li').hide().slice(0, 4).show();
     var askForLoad = false;
+    var selectedCuisine = [];
 
-    $( "#cuisine-load" ).click(function() {
+    $( '#cuisine-load' ).click(function() {
       if (!askForLoad){
         $('ul.cuisine-list li').show();
         askForLoad = true;
-        $("#cuisine-load").html("Hide");
+        $('#cuisine-load').html('Hide');
       } else {
-        $('ul.cuisine-list li').hide().slice(0, 4).show();;
+        $('ul.cuisine-list li').hide().slice(0, 4).show();
         askForLoad = false;
+        $('#cuisine-load').html('Load more');
       }
+    });
+
+    $('.cuisine-list li').click(function(){
+      $(this).toggleClass('selected');
+      selectedCuisine = [];
+      $( '.cuisine-list li' ).each(function() {
+        if ($(this).hasClass('selected')){
+          selectedCuisine[selectedCuisine.length] = $(this).attr('value');
+        }
+      });
+      console.log(selectedCuisine);
     });
 
     $('.price-slider').noUiSlider({
@@ -24,19 +37,19 @@
       }
     });
 
-    $(".price-slider").Link('lower').to($('.price-display-low'));
-    $(".price-slider").Link('upper').to($('.price-display-high'));
+    $('.price-slider').Link('lower').to($('.price-display-low'));
+    $('.price-slider').Link('upper').to($('.price-display-high'));
 
     $('.location-slider').noUiSlider({
       start: [15],
-      connect: "lower",
+      connect: 'lower',
       range: {
         'min': 0,
         'max': 30
       }
     });
 
-    $(".location-slider").Link('lower').to($('.location-display-low'));
+    $('.location-slider').Link('lower').to($('.location-display-low'));
 
 
 })();

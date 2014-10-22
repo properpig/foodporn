8a6b06df7ed03d356edebe5c6aeef392f60a8dd8
U San <properpig@gmail.com>
2014-10-22 21:48:11 +0800

    added share function

--- app/scripts/food.js
+++ app/scripts/food.js
@@ -36,6 +36,69 @@
           });
         });
 
+        $('.share-button').click(function(event) {
+          $(this).toggleClass('clicked');
+
+          // show the items if this is being clicked
+          if ($(this).hasClass('clicked')) {
+            // show a different button
+            $(this).attr('src', 'images/icons/share.svg');
+
+            $('.circle.share').show();
+            $('.circle.share').css({opacity:1.0});
+            // display them in a quadrant
+            var elems = $('.circle.share');
+            var increase = (Math.PI/4) * 2 / elems.length;
+            var x = 0, y = 0, angle = Math.PI/2, elem;
+
+            $('.circle.share').each(function() {
+              // elem.innerHTML = angle;
+              x = 155 * Math.cos(angle);
+              y = 170 * Math.sin(angle) + 50;
+              $(this).animate({
+                right: x,
+                top: y
+              });
+              angle -= increase;
+            });
+          } else {
+            // hide the items
+            $('.circle.share').animate({
+              right: 0,
+              top: 0,
+              opacity: 0
+            }, function() {
+              $('.circle.share').hide();
+              $('.share-button').attr('src', 'images/icons/share-o.svg');
+            })
+          }
+        });
+
+        var timeout;
+        $('.circle.share').click(function() {
+          var type = $(this).data('type');
+
+          // clear the old time out
+          clearTimeout(timeout);
+
+          $('.share-confirmation').show();
+          $('.share-confirmation .site').text(type);
+
+          $('.share-confirmation').animate({
+            bottom: 15,
+            opacity: 1.0
+          }, 500, function() {
+            timeout = setTimeout(function() {
+              $('.share-confirmation').animate({
+                bottom: -100,
+                opacity: 0.0
+              }, function() {
+                $('.share-confirmation').hide();
+              });
+            }, 1500);
+          });
+        });
+
       });
 
     }
